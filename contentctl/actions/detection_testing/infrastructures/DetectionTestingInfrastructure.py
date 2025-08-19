1fa107ebf26664c1fe5c97a56a4dc9a6ac719262
Gary Carr <gary@mx51.io>
2025-08-19 15:06:43 +1000

    fixing typo

--- contentctl/actions/detection_testing/infrastructures/DetectionTestingInfrastructure.py
+++ contentctl/actions/detection_testing/infrastructures/DetectionTestingInfrastructure.py
@@ -746,7 +746,7 @@ class DetectionTestingInfrastructure(BaseModel, abc.ABC):
          # run before the test data has been summarised.
         if not self._data_model_delay_complete:
             self.pbar.write("WAITING for 10 minutes for Data Model Acceleration...")
-            time.sleep(600) # 10 minutes
+            time.sleep(2) # 10 minutes
             self._data_model_delay_complete = True
 
         # First, check to see if this test has been skipped; log and return if so
